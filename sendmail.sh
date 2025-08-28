45a52895558f279b9894a1eca91653505acaca29
ultraUltei <146963449+ultraUltei@users.noreply.github.com>
2025-08-29 01:26:38 +0200

    Update sendmail.sh

--- sendmail.sh
+++ sendmail.sh
@@ -3,4 +3,5 @@ echo "From: host@mail.com" > msg.txt
 echo "Subject: Public IP Update" >> msg.txt
 echo "" >> msg.txt
 echo "Your Public IP has been changed to: "$1 >> msg.txt
-msmtp yourmail@mail.com < msg.txt  #change this line if needed
+# If you use a mail client other than msmtp, modify the line below
+msmtp yourmail@mail.com < msg.txt
