40ce19f31a5863143a4bfaa7cf9608802448d8f8
ultraUltei <146963449+ultraUltei@users.noreply.github.com>
2025-08-29 00:54:43 +0200

    Update publicip-daemon.c

--- publicip-daemon.c
+++ publicip-daemon.c
@@ -3,12 +3,14 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define OFFLINE_CYCLE 60*5
-#define ONLINE_CYCLE 60*10
+// defines
+#define OFFLINE_CYCLE 60*5 // offline cycle time
+#define ONLINE_CYCLE 60*10 // online cycle time
 
 #define SIZE 3
 #define IP_SIZE 16
 
+// prototypes
 int internet_access();
 void ip_checker();
 void mail_sender(char*);
@@ -22,28 +24,24 @@ int main(int argc, char* argv[]){
     if(!file) return 0;
 
     char * state = (char *) malloc(sizeof(char)*SIZE);
-    while(sleep(1), 1){
+    while(sleep(1), 1){ // wating for the 'up' interface state
         if(!fgets(state, SIZE, file)) return 0;
-        if(!strcmp(state, "up")){
+		rewind(file);
+        if(!strcmp(state, "up")){ // state is 'up'
         	free(state);
-        	fclose(file);
-            //controllo immediato all'avvio   
-            while (!internet_access()) sleep(5);
-            //controllo dell'accesso ad internet
-            while(ip_checker(), sleep(ONLINE_CYCLE), 1) if(!internet_access()) while(sleep(OFFLINE_CYCLE), 1) if(internet_access()) break;           
+        	fclose(file);   
+            while (!internet_access()) sleep(5); // this while waits for internet access
+			// the second while checks for the public IP and waits if there's no internet access
+            while(ip_checker(), sleep(ONLINE_CYCLE), 1) if(!internet_access()) while(sleep(OFFLINE_CYCLE), 1) if(internet_access()) break; 
         }
-        rewind(file);
     }
-}
-
-int internet_access(){
-	return !system("ping -c 1 -W 1 1.1.1.1");
 }
 
 void ip_checker(){    
     char ip[IP_SIZE];
-    if(!curl_ip(ip)) return;
+    if(!curl_ip(ip)) return; // gets the current IP
 
+	// old IP from the ip.txt file
 	FILE* fileIP = fopen("ip.txt", "r");
 	if(!fileIP) {
 		ip_file_creator();
@@ -51,15 +49,31 @@ void ip_checker(){
 	}
     
     char oldip[IP_SIZE];
-    fgets(oldip, IP_SIZE, fileIP);
+    fgets(oldip, IP_SIZE, fileIP); // this line reads the ip.txt file
     fclose(fileIP);
 
     ip[strcspn(ip, "\n")] = '\0';
     oldip[strcspn(oldip, "\n")] = '\0';
 
-    if(strcmp(ip, oldip)) mail_sender(ip);
+    if(strcmp(ip, oldip)) mail_sender(ip); // if the IPs are different, call the mail_sender() function
+}
+void mail_sender(char* ip){
+    char command[64];
+    snprintf(command, sizeof(command), "sh ./sendmail.sh %s", ip);
+    if(system(command)) return; // this line runs the script
+
+    // ip.txt update
+    FILE *fileIP = fopen("ip.txt", "w");
+    if (!fileIP) return;
+    fprintf(fileIP, "%s", ip);
+    fclose(fileIP);
 }
 
+
+// other functions
+int internet_access(){
+	return !system("ping -c 1 -W 1 1.1.1.1");
+}
 int curl_ip(char* ip){
 	FILE* curl = popen("curl -s ident.me", "r");
 	if(!curl) return 0;
@@ -70,21 +84,7 @@ int curl_ip(char* ip){
 	pclose(curl);
 	return 0;
 }
-
 void ip_file_creator(){
 	FILE* file = fopen("ip.txt", "w");
 	if(file) fclose(file);
-}
-
-void mail_sender(char* ip){
-    //invio email del nuovo ip
-    char command[64];
-    snprintf(command, sizeof(command), "sh ./sendmail.sh %s", ip);
-    if(system(command)) return;
-
-    //aggiornamento del file ip.txt
-    FILE *fileIP = fopen("ip.txt", "w");
-    if (!fileIP) return;
-    fprintf(fileIP, "%s", ip);
-    fclose(fileIP);
 }
